e74a709c8539d5d1dddf797cf894779381431191
himanshukrabc <himanshukrabc@gmail.com>
2024-12-27 13:17:03 +0530

    First

--- apps/dbWorker/src/index.ts
+++ apps/dbWorker/src/index.ts
@@ -22,12 +22,13 @@ const server = new Server(httpServer, {
 });
 
 const queue = new Queue();
-const consumer = queue.getKafkaInstance().consumer({ groupId: "dbWorker" });
 
 server.on("connect", async(socket)=>{
+  const queue = new Queue();
   socket.on("topic",async(data,ack)=>{
     try{
       const {topic}:{topic:string} = data;
+      const consumer = queue.getKafkaInstance().consumer({ groupId: "dbWorker" });
       await consumer.subscribe({topic});
       ack({success:true});
     }
@@ -35,32 +36,38 @@ server.on("connect", async(socket)=>{
       ack({success:false});
     }
   });
-  socket.on("getMessage", async(data,ack)=>{
+
+
+  socket.on("getMessages", async(data)=>{
     try {
-      console.log("Message");
+      console.log("get Messages");
       const id: string = data.id;
       const user = await prisma.user.findUnique({
         where: { id },
         select: { offset: true },
       });
+  
       console.log(id);
       if (!user || !user.hasOwnProperty("offset")) {
-        ack({ success:false, message: "user not found" });
+        socket.emit("response",{ success:false, message: "user not found" });
+        return;
       }
+
       console.log(user);
       const messages: any[] = [];
-  
+
       const admin = queue.getKafkaInstance().admin();
       await admin.connect();
       const topicOffsets = await admin.fetchTopicOffsets(id);
       await admin.disconnect();
       const latestOffset = topicOffsets.find(({ partition }) => partition == 0)?.high;
       if (!latestOffset) {
-        ack({success:true});
-        socket.emit("newMessages",{messages});
+        socket.emit("response",{success:true, messages});
+        return;
       }
+
       console.log(topicOffsets);
-  
+
       const consumer = queue.getKafkaInstance().consumer({ groupId: id + "-getMessage" });
       await consumer.connect();
       await consumer.subscribe({ topic: id });
@@ -77,7 +84,7 @@ server.on("connect", async(socket)=>{
           if (message.offset === (parseInt(latestOffset) - 1).toString()) {
             continueConsuming = false;
             await consumer.disconnect();
-            res.status(200).json({ messages });
+            socket.emit("response",{success:true, messages });
           }
         },
       });
@@ -86,75 +93,19 @@ server.on("connect", async(socket)=>{
       console.log("seek done");
       if (continueConsuming) {
         await consumer.disconnect();
-        return res.status(200).json({ success: true, messages });
+        socket.emit("response",{success:true, messages });
       }
     } catch (err) {
       console.log(err);
-      return res.status(500).json({ message: "Internal server error" });
+      socket.emit("response",{success:false, message:"Internal server error" });
     }
   })
 });
 
-app.post("/getMessage", async (req: Request, res: Response): Promise<any> => {
-  try {
-    console.log("Message");
-    const id: string = req.body.id;
-    const user = await prisma.user.findUnique({
-      where: { id },
-      select: { offset: true },
-    });
-    console.log(id);
-    if (!user || !user.hasOwnProperty("offset")) {
-      return res.status(404).send({ message: "user not found" });
-    }
-    console.log(user);
-    const messages: any[] = [];
-
-    const admin = queue.getKafkaInstance().admin();
-    await admin.connect();
-    const topicOffsets = await admin.fetchTopicOffsets(id);
-    await admin.disconnect();
-    const latestOffset = topicOffsets.find(({ partition }) => partition == 0)?.high;
-    if (!latestOffset) {
-      return res.status(200).json({ messages });
-    }
-    console.log(topicOffsets);
-
-    const consumer = queue.getKafkaInstance().consumer({ groupId: id + "-getMessage" });
-    await consumer.connect();
-    await consumer.subscribe({ topic: id });
-    
-    let continueConsuming = true;
-    console.log("starting to run");
-    consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
-        if (!message.value) {
-          return;
-        }
-        console.log(message.value.toString());
-        messages.push(JSON.parse(message.value.toString()));
-        if (message.offset === (parseInt(latestOffset) - 1).toString()) {
-          continueConsuming = false;
-          await consumer.disconnect();
-          res.status(200).json({ messages });
-        }
-      },
-    });
-    console.log("starting seek");
-    consumer.seek({topic: id,partition: 0,offset: user ? user.offset.toString() : "0"});
-    console.log("seek done");
-    if (continueConsuming) {
-      await consumer.disconnect();
-      return res.status(200).json({ success: true, messages });
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "Internal server error" });
-  }
-});
-
 httpServer.listen(PORT, async () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  const queue = new Queue();
+  const consumer = queue.getKafkaInstance().consumer({ groupId: "dbWorker" });
   await consumer.connect();
   const users = await prisma.user.findMany({ select: { id: true } });
   const topics = users.map((user) => user.id);
@@ -165,6 +116,7 @@ httpServer.listen(PORT, async () => {
     })
   );
   await consumer.subscribe({ topics });
+  console.log("consumer starting to read messages");
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       if (!message.value) {
